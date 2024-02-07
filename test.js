a049fb0ba0e1c7e40e6b22da941f0bfbd7e1724a
anilMichel <anilkumarvoonna123456@gmail.com>
2024-02-07 18:48:12 +0530

    differnece bewtween the previous and modified one in the file

--- test.js
+++ test.js
@@ -2,3 +2,5 @@ console.log("Anil Kumar")
 //after commiting the file i add some to the previous file 
 console.log("added another one console log")
 //we had done the committing without adding into the stagingn area
+let str = "Anil Kumar"
+//gfgddfds
