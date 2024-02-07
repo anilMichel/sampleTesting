227afaa305011b053de1c48fa6eb5b77b6f71d43
anilMichel <anilkumarvoonna123456@gmail.com>
2024-02-07 18:18:12 +0530

    added a comment about skipping the staging area

--- test.js
+++ test.js
@@ -1,3 +1,4 @@
 console.log("Anil Kumar")
 //after commiting the file i add some to the previous file 
 console.log("added another one console log")
+//we had done the committing without adding into the stagingn area
