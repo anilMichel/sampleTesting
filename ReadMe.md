b89f474fb5856453faf060df383e8a8a19e161fb
anilMichel <anilkumarvoonna123456@gmail.com>
2024-02-07 18:56:38 +0530

    adding 2 files and one is modified

---
+++ ReadMe.md
@@ -0,0 +1,1 @@
+This is the practice of git and working on the git.
--- test.js
+++ test.js
@@ -4,3 +4,6 @@ console.log("added another one console log")
 //we had done the committing without adding into the stagingn area
 let str = "Anil Kumar"
 //gfgddfds
+
+//adding one more comment to the prevoisusly commited file.
+console.log("one more modification")
